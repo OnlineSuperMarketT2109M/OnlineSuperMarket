d883f204cca7dd2757d158536907df4e5b4c1f94
nguyenkhanh08112003 <91303308+nguyenkhanh08112003@users.noreply.github.com>
2023-05-09 10:22:03 +0700

    chỉnh mệnh giá $ -> đ

---
+++ OnlineSuperMarket/wwwroot/ClientAssets/js/login.js
@@ -0,0 +1,21 @@
+﻿const loginForm = document.querySelector(".loginForm");
+const overlay = document.querySelector("#overlay");
+const signIn = document.querySelector(".headerTop--list--signin");
+const closeLoginForm = document.querySelector("#closeLoginForm");
+
+function displayLoginForm() {
+    loginForm.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+signIn.onclick = function () {
+    displayLoginForm();
+}
+
+overlay.onclick = function () {
+    displayLoginForm();
+}
+
+closeLoginForm.onclick = function () {
+    displayLoginForm();
+}
